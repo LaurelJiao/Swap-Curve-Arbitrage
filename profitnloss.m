0e7daefcb677e1266f18e2d79a5198a8685982f5
Laurel Jiao <ruixinjiao@gmail.com>
2017-02-13 20:50:44 +0800

    Changed hedge
    1. Changed the calculation of delta, by replacing dc to calculate bondprice.
    2. 'calculate_return' needs to be changed again from dc to bondprice!

--- profitnloss.m
+++ profitnloss.m
@@ -1,14 +1,14 @@
-function f = profitnloss(tool_swap, tool_maturity, dc_tool,target_swap, target_maturity, dc_target,error,LIBOR, pars)
+function f = profitnloss(tool_swap, tool_maturity, dc_tool,target_swap, target_maturity, dc_target,error,LIBOR, pars, rts)
 	for i=1:1826
-		[long short] = calculate_return(tool_swap(i),dc_tool(i:22*6*2*3+i,1),tool_maturity,target_swap(i),dc_target(i:22*6*2*3+i,1),target_maturity,LIBOR(i+6*22));
+		[long, short] = calculate_return(tool_swap(i),dc_tool(i:22*6*2*3+i,1),tool_maturity,target_swap(i),dc_target(i:22*6*2*3+i,1),target_maturity,LIBOR(i+6*22));
 	% Calculate delta
 		sum = 0;
 		for j = 1:2*tool_maturity
 			B = 1/pars(6*j*22+i-1,1)*(1-exp(-pars(6*j*22+i-1,1)*(tool_maturity)));
-        	sum = sum - B*dc_tool(6*j*22+i-1);
+        	sum = sum - B*bondPrice(pars(6*j*22+i-1,:),rts(6*j*22+i-1,1),0.5*j);
     	end 
-    	X = target_swap(i)/2 * sum- 1/pars(i,1)*(1-exp(-pars(i,1)*(target_maturity)))*dc_target(i);
-    	Y = tool_swap(i)/2 * sum - 1/pars(i,1)*(1-exp(-pars(i,1)*(tool_maturity)))*dc_tool(i);
+    	X = target_swap(i)/2 * sum- 1/pars(i,1)*(1-exp(-pars(i,1)*(target_maturity)))*bondPrice(pars(i,:),rts(i),target_maturity);
+    	Y = tool_swap(i)/2 * sum - 1/pars(i,1)*(1-exp(-pars(i,1)*(tool_maturity)))*bondPrice(pars(i,:),rts(i),tool_maturity);
     	delta = X/Y;
 		if error(i) > 0.25
 			% long tool swap, short target swap
